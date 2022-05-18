a615ec959453cffae0d50fbf28e5fcf2c8274fc1
yejue <yejue@yjstudy1.com>
2022-05-18 14:41:19 +0800

    [add] 添加启停，快捷键响应与演奏

--- client/views.py
+++ client/views.py
@@ -1,26 +1,87 @@
-from PyQt5.QtWidgets import QWidget, QAbstractItemView
+import system_hotkey
+from PyQt5.QtWidgets import QWidget, QTableWidgetItem, QAbstractItemView, QHeaderView
+from PyQt5.QtCore import pyqtSignal
 
-from genshin_assistant.play import PlaySong
+from genshin_assistant.play import Play
 from .ui.lyre_assitant import Ui_lyre_assitant
+from . import constant
 
 
 class AssistantView(QWidget, Ui_lyre_assitant):
     """风琴助手视窗"""
+    hotkey_signal = pyqtSignal(str)  # 热键发送信号
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)  # 界面初始化
-        self.play = PlaySong()  # 实例化风琴演奏类
+        self.play = Play()  # 实例化风琴演奏类
         self.set_song_list()
         self.set_addition_ui()  # Designer 额外的 UI 设置
 
+        self.play_one = system_hotkey.SystemHotkey()  # 弹奏热键
+        self.set_slot_function()  # 绑定槽函数
+
     def set_addition_ui(self):
         """额外的 UI 设置"""
-        self.lw_song.setEditTriggers(QAbstractItemView.NoEditTriggers)  # 歌曲列表不可编辑
+
+        # 初始按键状态
+        self.pb_stop.setEnabled(False)  # 停止按键置灰
+
+        # 歌曲列表设置
+        self.tw_song.setEditTriggers(QAbstractItemView.NoEditTriggers)  # 列表不可编辑
+        self.tw_song.hideColumn(1)  # 隐藏歌曲路径字段
+        self.tw_song.horizontalHeader().setVisible(False)  # 隐藏表头
+        self.tw_song.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)  # 列宽重新分配
+
+    def set_slot_function(self):
+        """统一设置绑定槽函数"""
+        self.pb_play.clicked.connect(self.play_start)  # 开始按钮
+        self.pb_stop.clicked.connect(self.play_stop)  # 停止按钮
 
     def set_song_list(self):
         """获取曲目表"""
         song_list_obj = self.play.get_song_list()
-        song_list = [item.name for item in song_list_obj]
+        for index, obj in enumerate(song_list_obj):
+            name_item = QTableWidgetItem(obj.name)
+            name_path = QTableWidgetItem(str(obj.absolute()))
 
-        self.lw_song.addItems(song_list)
+            self.tw_song.setRowCount(self.tw_song.rowCount()+1)  # 将歌曲列表更新到表格
+            self.tw_song.setItem(index, 0, name_item)
+            self.tw_song.setItem(index, 1, name_path)
+
+    def hotkey_pressed_event(self, hotkey):
+        """热键相应事件"""
+        # 检验演奏列表是否为空，空则取消注册快捷键，将停止按键置灰，将 start 置白
+        if not self.play.play_list:
+            self.play_one.unregister(constant.PLAY_HOT_KEY)
+            self.pb_stop.setEnabled(False)
+            self.pb_play.setEnabled(True)
+        else:
+            self.play.play_one_beat()
+
+    def play_start(self):
+        """演奏开始"""
+        # 1. 将演奏歌曲按键加载到演奏列表
+        current_row = self.tw_song.currentRow()
+        if current_row == -1:
+            return
+        song_path = self.tw_song.item(current_row, 1).text()
+        self.play.load_song(song_path)
+        # 2. 更新按键状态
+        self.pb_play.setEnabled(False)
+        self.pb_stop.setEnabled(True)
+
+        self.play_one.register(constant.PLAY_HOT_KEY, callback=self.hotkey_pressed_event)  # 注册弹奏热键
+
+    def play_stop(self):
+        """演奏停止"""
+        self.play_one.unregister(constant.PLAY_HOT_KEY)  # 取消注册弹奏热键
+        self.play.play_list = []  # 清空按键列表
+
+        self.pb_play.setEnabled(True)
+        self.pb_stop.setEnabled(False)
+
+    def closeEvent(self, a0) -> None:
+        """窗口关闭事件"""
+        if constant.PLAY_HOT_KEY in self.play_one.keybinds:
+            self.play_one.unregister(constant.PLAY_HOT_KEY)  # 取消注册弹奏热键
--- genshin_assistant/play.py
+++ genshin_assistant/play.py
@@ -1,54 +1,8 @@
 import json
-import time
 from pathlib import Path
 
 from libs.simulation import Simulator
 from . import settings
-
-
-class PlaySong:
-    """演奏类"""
-    song_dir = settings.SONG_DIR
-
-    @staticmethod
-    def get_song_conf(filepath):
-        """获取演奏歌曲对象"""
-        with open(filepath) as f:
-            song_conf = json.loads(f.read())
-        return song_conf
-
-    def get_song_list(self):
-        """获取歌曲列表"""
-        song_list = Path(self.song_dir).iterdir()
-        return list(song_list)
-
-    def play(self, song):
-        """演奏"""
-        time.sleep(3)
-
-        song_conf = self.get_song_conf(song)
-        press_list = song_conf["press_list"]
-        beat_frequency = song_conf["beat_frequency"]
-        simulator = Simulator()  # 实例化模拟按键类
-
-        while len(press_list) > 0:
-            per_press = press_list.pop(0)
-            per_press_list = list(per_press)  # 将每一个按键拆分成列表
-            for k in per_press_list:
-                simulator.send_key(k)
-            time.sleep(beat_frequency)  # 按照节拍时间休眠
-
-    def run(self):
-        while True:
-            song_obj_list = self.get_song_list()
-            song_list = [item.name for item in song_obj_list]
-
-            for i, song in enumerate(song_list):
-                print(i, song)
-
-            select = int(input("请选择歌曲>>>"))
-            select_song = song_obj_list[select]
-            self.play(select_song)
 
 
 class Play:
@@ -57,6 +11,7 @@ class Play:
 
     def __init__(self):
         self.play_list = []  # 演奏按键列表
+        self.simulator = Simulator()
 
     def get_song_list(self):
         """返回歌曲路径对象列表"""
@@ -72,4 +27,8 @@ class Play:
 
     def play_one_beat(self):
         """演奏一个节拍"""
-        pass
+        if self.play_list:
+            key = self.play_list.pop(0)
+            print(key)
+            for k in list(key):
+                self.simulator.send_key(k)
